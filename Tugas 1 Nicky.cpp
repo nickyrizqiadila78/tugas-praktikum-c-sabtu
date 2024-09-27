11b86f29d6a1bdb5a2acebc5ce503d90bc051b1c
nickyrizqiadila78 <nickyrizqy78@gmail.com>
2024-09-27 22:01:55 +0700

    Add files via upload

---
+++ Tugas 1 Nicky.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char nim = "2441034";
+    char nama[50] = "nicky rizqi adila";
+    char jurusan[50] = "Teknik Informatika";
+
+    printf("NIM: %d\n", nim);
+    printf("Nama: %s\n", nama);
+    printf("Jurusan: %s\n", jurusan);
+
+    return 0;
+}
